05957342925af4ce96217c92acbbac923066c101
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-07-10 19:52:18 +0300

    Auth exceptions

--- lib/services/auth/auth_exception.dart
+++ lib/services/auth/auth_exception.dart
@@ -1,4 +1,55 @@
-abstract class AuthException
+// login
+class UserNotFoundException implements Exception
+{
+
+}
+
+class WrongPasswordException implements Exception
+{
+  
+}
+
+class EmptyMailException implements Exception
+{
+
+}
+
+class EmptyPasswordException implements Exception
+{
+  
+}
+
+
+
+//register
+class WeakPasswordException implements Exception
+{
+  
+}
+
+class EmailIsUsedException implements Exception
+{
+  
+}
+
+class InvalidMailException implements Exception
+{
+  
+}
+
+class NotAllowedOperationException implements Exception
+{
+  
+}
+
+
+//generic
+class GenericException implements Exception
+{
+  
+}
+
+class UserNotLoggedInException implements Exception
 {
   
 }
