8cfe4b1907b321878de8c3b49650c5381cad1ba4
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-07-10 19:52:30 +0300

    Auth events

--- lib/services/auth/bloc/auth_event.dart
+++ lib/services/auth/bloc/auth_event.dart
@@ -1,10 +1,41 @@
-abstract class AuthEvent
-{
-  
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+abstract class AuthEvent {
+  const AuthEvent();
 }
 
+class AuthInitializeEvent extends AuthEvent {
+  const AuthInitializeEvent();
+}
 
-class AuthInitializeEvent extends AuthEvent
-{
-  
+class AuthLogInEvent extends AuthEvent {
+  final String email;
+  final String password;
+
+  const AuthLogInEvent(this.email, this.password);
+}
+
+class AuthLogOutEvent extends AuthEvent {
+  const AuthLogOutEvent();
+}
+
+class AuthSendEmailVerificationEvent extends AuthEvent {
+  const AuthSendEmailVerificationEvent();
+}
+
+class AuthRegisterEvent extends AuthEvent {
+  final String email;
+  final String password;
+
+  const AuthRegisterEvent(this.email, this.password);
+}
+
+class AuthShouldRegisterEvent extends AuthEvent {
+  const AuthShouldRegisterEvent();
+}
+
+class AuthForgotPasswordEvent extends AuthEvent {
+  final String? email;
+  const AuthForgotPasswordEvent({this.email});
 }
