ca5294e4257842c809c6bebeba78a40ce8b94ed6
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-07-12 19:49:58 +0300

    Login view design

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infohive/widgets/login/forgot_password_label.dart';
 
 import '../../services/auth/bloc/auth_bloc.dart';
 import '../../services/auth/bloc/auth_state.dart';
+
+import '../../widgets/login/login_widgets.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -31,9 +34,58 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return BlocListener<AuthBloc, AuthState>(
-      listener: (context, state){},
-      //TODO Login view design and bloc
+      listener: (context, state) {
+        //TODO bloc listener
+      },
+      child: Scaffold(
+          body: SizedBox(
+        height: height,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: -height * .15,
+              right: -MediaQuery.of(context).size.width * .4,
+              child: const BezierContainer(),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: height * .2),
+                    title(),
+                    const SizedBox(height: 50),
+                    emailPasswordWidget(
+                      email: _emailController,
+                      password: _passwordController,
+                    ),
+                    const SizedBox(height: 20),
+                    submitButton(
+                      context,
+                      onTap: () {
+                        //TODO Submit tap handling
+                      },
+                    ),
+                    forgotPasswordLabel(
+                      onTap: () {
+                        //TODO Forgot password tap hadnling
+                      },
+                    ),
+                    SizedBox(height: height * .055),
+                    createAccountLabel(onTap: () {
+                      //TODO Create account tap handling
+                    }),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
