8c97af8db4e62c783ea217550e245f2b97abf966
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-07-13 16:22:44 +0300

    register view

--- lib/views/auth/register_view.dart
+++ lib/views/auth/register_view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infohive/services/auth/bloc/auth_bloc.dart';
+import 'package:infohive/services/auth/bloc/auth_event.dart';
 import 'package:infohive/services/auth/bloc/auth_state.dart';
+import 'package:infohive/widgets/register/login_account_label.dart';
+
+import '../../widgets/login/bezier_container.dart';
+import '../../widgets/register/email_password.dart';
+import '../../widgets/register/submit_button.dart';
+import '../../widgets/register/title.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -11,20 +18,24 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _usernameController;
   late final TextEditingController _emailController;
   late final TextEditingController _passwordController;
   late final TextEditingController _confirmPasswordController;
 
   @override
   void initState() {
+    _usernameController = TextEditingController();
     _emailController = TextEditingController();
     _passwordController = TextEditingController();
     _confirmPasswordController = TextEditingController();
+
     super.initState();
   }
 
   @override
   void dispose() {
+    _usernameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     _confirmPasswordController.dispose();
@@ -34,10 +45,52 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return BlocListener<AuthBloc, AuthState>(
-      listener: (context, state) {},
-      child: const Scaffold(
-        // TODO Register View Design and bloc
+      listener: (context, state) {
+        //TODO exceptions
+      },
+      child: Scaffold(
+        body: SizedBox(
+          height: height,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: -MediaQuery.of(context).size.height * .15,
+                right: -MediaQuery.of(context).size.width * .4,
+                child: const BezierContainer(),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: height * .2),
+                      title(),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      emailPasswordWidget(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      submitButton(context, onTap: () { 
+                        //TODO onTap
+                       }),
+                      loginAccountLabel(onTap: (){
+                      context
+                          .read<AuthBloc>()
+                          .add(const AuthLogOutEvent());
+                      }),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
