6424372852d7a8d751fcce01c4dd59b381d23822
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-07-13 16:22:09 +0300

    new color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color primary = Color.fromARGB(225, 255, 221, 0);
 const Color secondary = Color.fromARGB(255, 255, 162, 0);
+const Color textWhite = Color.fromARGB(255, 245, 245, 245);
